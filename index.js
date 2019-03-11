2d61e52b8b38fb301f50d1bdf208b862f75db47c
Dana Simmons <dana@simmons-audio.com>
2019-03-11 13:16:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here:
+
+function driversWithRevenueOver(driver, revenue) {
+    return driver.filter(d => d.revenue > revenue)
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+    const filteredDrivers = driversWithRevenueOver(drivers, revenue)
+    return filteredDrivers.map(d => d.name)
+}
+
+function exactMatch(drivers,filterObj) {
+    return drivers.filter(function(d) {
+        let fKey = Object.keys(filterObj)[0]
+        return d[fKey] === filterObj[fKey]
+    })
+}
+
+function exactMatchToList(drivers,filterObj) {
+    return exactMatch(drivers,filterObj).map(d=>d.name)
+}
